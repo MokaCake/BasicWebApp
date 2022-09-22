1cc7b328ccd5c4028345e777105b3cf822a15b87
Karen <karenson@andrew.cmu.edu>
2022-09-22 15:49:51 -0400

    largest

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -16,6 +16,21 @@ public class QueryProcessor {
             return Integer.toString(Integer.parseInt(parts[3]) + Integer.parseInt(parts[5]));
         }
 
+        if (query.toLowerCase().contains("the largest")) {
+            String[] sub = query.split(":");
+            String[] parts = sub[2].split(",");
+            int[] parts_num = new int[parts.length];
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < parts.length; i ++){
+                parts_num[i] = Integer.parseInt(parts[i]);
+            }
+            for (int i = 0; i < parts.length; i ++){
+                if (max < parts_num[i]){
+                    max = parts_num[i];
+                }
+            }
+            return Integer.toString(max);
+        }
         
 
         return "";
